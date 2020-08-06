0dd8918d7a517d5e8a06bc2b8960a41e246b22cf
Chandler Lutz <chandler.lutz@gmail.com>
2020-08-06 06:27:49 -0700

    Model for msgs and associated migrations

--- msgs/tests.py
+++ msgs/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from msgs.models import Msg 
 
 from msgs.views import home_page
 
@@ -15,6 +16,23 @@ class HomePageTest(TestCase):
         
                         
 
-    
+class MsgModelTest(TestCase):
 
+    def test_saving_and_retrieving_msgs(self):
+        first_msg = Msg()
+        first_msg.text = 'The first (ever) message'
+        first_msg.save()
 
+        second_msg = Msg()
+        second_msg.text = 'Msg the second'
+        second_msg.save()
+
+        saved_msgs = Msg.objects.all()
+        self.assertEqual(saved_msgs.count(), 2)
+
+        first_saved_msg = saved_msgs[0]
+        second_saved_msg = saved_msgs[1]
+        self.assertEqual(first_saved_msg.text, 'The first (ever) message')
+        self.assertEqual(second_saved_msg.text, 'Msg the second')
+        
+        
