6a829c9de41859acbedc83cb147840d1a2919583
Chandler Lutz <chandler.lutz@gmail.com>
2020-08-20 08:30:18 -0700

    set STATIC_ROOT in settings and disable admin

--- supermapp/settings.py
+++ supermapp/settings.py
@@ -27,11 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -119,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
